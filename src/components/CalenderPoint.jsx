fe4c88e37249ec91677ec5087f36ee3af42bbc98
Bytistan <al_dokan_20@hotmail.com>
2025-04-26 15:01:07 +0300

    Calender interface is done

---
+++ src/components/CalenderPoint.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import BatteryIcon from "../assets/icons/battery.svg";
+
+export default function CalendarPoint({ dateNumber, monthName, pointNumber = 0 }) {
+    return (
+        <div className="w-100 d-flex justify-content-start align-items-center gap-4 px-4 py-3 bottom-border-gray">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <p className="fs-2 fw-bold text-dark">{dateNumber}</p>
+                <p className="text-gray fw-bold">{monthName}</p>
+            </div>
+            <div className="d-flex justify-content-start align-items-center gap-2">
+                {Array.from({ length: pointNumber }).map((_, index) => (
+                    <img key={index} src={BatteryIcon} width="48" alt="Battery Icon" />
+                ))}
+            </div>
+        </div>
+    );
+}
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,15 +1,24 @@
-import React, { useState }  from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import IconButton from '../components/IconButton';
 
 import BackIcon from '../assets/icons/back.svg';
 
+import CalendarPoint from '../components/CalenderPoint';
+
 export default function Calendar() {
     return (
         <div className="page-background d-flex flex-column vh-100">
 
             <main className="flex-grow-1 overflow-auto p-4 d-flex flex-column">
+                <div className="gap-4 d-flex justify-content-start align-items-start flex-column bg-white flex-grow-1 rounded-4">
+                    <CalendarPoint
+                        pointNumber={5}
+                        monthName={"may"}
+                        dateNumber={5}
+                    />
+                </div>
             </main>
 
             <footer className="d-flex justify-content-start align-items-center p-4" style={{ height: '100px' }}>
